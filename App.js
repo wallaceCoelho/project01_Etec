8e9a7890123cc84de28d7c260d7f19ef64ae8745
wallaceCoelho <wacoelho.dev@gmail.com>
2022-08-13 01:33:24 +0000

    criando layout basico

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';//importação de componentes
 
-export default function App() {
+export default function App() {//Permite que essa função seja exposta para toda a aplicação com retorno
   return (
+	  //Posso fechar as própias tags utiliando a / (<TextInput />)
+	  //As tags no React Native sempre iniciam com letras maiúsculas
+	  //View é como se fosse as <div> do HTML, a diferença é que preciso ter uma View principal para mostrar tudo e só podemos englobar uma View dentro de outro View 
     <View style={styles.container}>
-      <Text>Olá mundo!</Text>
-      <StatusBar style="auto" />
+		<View style={styles.bloco}>
+      		<Text style={styles.titulo}>Calculadora Simples!</Text>
+      	</View>
+		<View style={styles.bloco}>
+			<Text> Valor 1: </Text>
+			<TextInput style={styles.input}/>
+		</View>
+		<View style={styles.bloco}>
+			<Text> Valor 2: </Text>
+			<TextInput style={styles.input}/>
+		</View>
+		<View style={styles.bloco}>
+			<TouchableOpacity style={styles.botao}>
+				<Text style={styles.textoBotao}>Somar</Text>
+			</TouchableOpacity>
+		</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItens: 'center',
+  	},
+	bloco: {
+		marginTop: 10,
+		width: '80%',
+		marginLeft:'10%'
+	},
+	titulo: {
+		fontSize: 30,
+		textAlign: 'center'
+	},
+	input: {
+		borderWidth: 2,
+		fontSize: 20,
+		borderRadius:10
+	},
+	botao: {
+		backgroundColor: '#000',
+		padding: 10,
+		width: 80,
+		margin: 'auto',
+		borderRadius: 10
+	},
+	textoBotao: {
+		color: '#fff',
+		textAlign: 'center',
+		fontSize: 20
+	},
 });
