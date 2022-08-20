16ac18bbdf20db94f9244162e11e6fad0b115d4f
wallaceCoelho <wacoelho.dev@gmail.com>
2022-08-19 22:49:32 -0300

    Adiconando função soma

--- App.js
+++ App.js
@@ -1,32 +1,65 @@
-import react from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';//importação de componentes
 
 export default function App() {//Permite que essa função seja exposta para toda a aplicação com retorno
-  return (
+  
+	//REACT:
+	//usando react js Hooks usestate
+	
+	const [valor1, setValor1] = useState(20);
+	const [valor2, setValor2] = useState(3);
+	const [resultado, setResultado] = useState();
+
+	function somar(){
+		let res = parseFloat(valor1) + parseFloat(valor2);
+		setResultado(res);
+	}
+	return (
 	  //Posso fechar as própias tags utiliando a / (<TextInput />)
 	  //As tags no React Native sempre iniciam com letras maiúsculas
 	  //View é como se fosse as <div> do HTML, a diferença é que preciso ter uma View principal para mostrar tudo e só podemos englobar uma View dentro de outro View 
-    <View style={styles.container}>
+    
+//Marcação de texto:	
+	<View style={styles.container}>
 		<View style={styles.bloco}>
       		<Text style={styles.titulo}>Calculadora Simples!</Text>
       	</View>
 		<View style={styles.bloco}>
 			<Text> Valor 1: </Text>
-			<TextInput style={styles.input}/>
+			<TextInput 
+				style={styles.input}
+				value= {valor1}
+				onChangeText={(valor) => setValor1(valor)}
+				keyboardType="numeric"
+			/>
 		</View>
 		<View style={styles.bloco}>
 			<Text> Valor 2: </Text>
-			<TextInput style={styles.input}/>
+			<TextInput 
+				style={styles.input}
+				value= {valor2}
+				onChangeText={(valor) => setValor2(valor)}
+				keyboardType="numeric"
+			/>
 		</View>
 		<View style={styles.bloco}>
-			<TouchableOpacity style={styles.botao}>
+			<TouchableOpacity 
+				style={styles.botao}
+				onPress = {somar}>
+			
 				<Text style={styles.textoBotao}>Somar</Text>
 			</TouchableOpacity>
+		</View>
+		<View style={styles.bloco}>
+			<Text style={styles.titulo}>
+				Resultado: {resultado}
+			</Text>
 		</View>
     </View>
   );
 }
 
+//Estilização:
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
